6c3d8a7eaf7d1cf4dc59e2ef5ee5f718e10852da
YanniTheDev <71956500+YanniTheDev@users.noreply.github.com>
2025-01-07 16:15:02 +1300

    everything works! :fire:

--- filesorter.py
+++ filesorter.py
@@ -3,6 +3,8 @@ import shutil
 
 from tqdm import tqdm
 
+import time
+
 # Specifying source paths where the unsorted files are
 with open("sourcepath.txt") as source_path:
     source = source_path.readline()
@@ -16,33 +18,24 @@ with open("destinationpath.txt") as destination_path:
 # Separating the destinations into each individual destination
 destinations = destinations.split(",")
 
-# Displaying the progress bar
-for i in tqdm (range(len(source_files)), desc="Sorting..."):
-
-    try:
-        # Moving files
-        for file in source_files:
-            if file.endswith(".pdf"):
-                shutil.move(os.path.join(source, file), os.path.join(destinations[0], file))
-            elif file.endswith(".exe") or file.endswith(".msi"):
-                shutil.move(os.path.join(source, file), os.path.join(destinations[1], file))
-            elif file.endswith(".mp3"):
-                shutil.move(os.path.join(source, file), os.path.join(destinations[2], file))
-            elif file.endswith(".zip"):
-                shutil.move(os.path.join(source, file), os.path.join(destinations[3], file))
-            elif file.endswith(".txt"):
-                shutil.move(os.path.join(source, file), os.path.join(destinations[4], file))
-            elif file.endswith(".mp4"):
-                shutil.move(os.path.join(source, file), os.path.join(destinations[5], file))
-            elif file.endswith(".png"):
-                shutil.move(os.path.join(source, file), os.path.join(destinations[6], file))
-    except FileNotFoundError as e:
-        '''
-        We break out of this because all the files have been sorted, but because of the progress bar, it repeats
-        the file sorting system again
-        '''
-        print(e)
-        print("Files Sorted!")
-        break
+# Moving files
+for file in tqdm(source_files):
+    if file.endswith(".pdf"):
+        shutil.move(os.path.join(source, file), os.path.join(destinations[0], file))
+    elif file.endswith(".exe") or file.endswith(".msi"):
+        shutil.move(os.path.join(source, file), os.path.join(destinations[1], file))
+    elif file.endswith(".mp3"):
+        shutil.move(os.path.join(source, file), os.path.join(destinations[2], file))
+    elif file.endswith(".zip"):
+        shutil.move(os.path.join(source, file), os.path.join(destinations[3], file))
+    elif file.endswith(".txt"):
+        shutil.move(os.path.join(source, file), os.path.join(destinations[4], file))
+    elif file.endswith(".mp4"):
+        shutil.move(os.path.join(source, file), os.path.join(destinations[5], file))
+    elif file.endswith(".png"):
+        shutil.move(os.path.join(source, file), os.path.join(destinations[6], file))
     
-print("Files sorted!")
+# Adding a little delay so that the Files Sorted! message comes after the 100%
+time.sleep(0.1)
+
+print("\nFiles sorted!")
