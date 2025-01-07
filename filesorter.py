c66c4a88f4e97307c6debcc86cc1d8289a382276
YanniTheDev <71956500+YanniTheDev@users.noreply.github.com>
2025-01-07 16:06:19 +1300

    loading bug

--- filesorter.py
+++ filesorter.py
@@ -19,21 +19,30 @@ destinations = destinations.split(",")
 # Displaying the progress bar
 for i in tqdm (range(len(source_files)), desc="Sorting..."):
 
-    # Moving files
-    for file in source_files:
-        if file.endswith(".pdf"):
-            shutil.move(os.path.join(source, file), os.path.join(destinations[0], file))
-        elif file.endswith(".exe") or file.endswith(".msi"):
-            shutil.move(os.path.join(source, file), os.path.join(destinations[1], file))
-        elif file.endswith(".mp3"):
-            shutil.move(os.path.join(source, file), os.path.join(destinations[2], file))
-        elif file.endswith(".zip"):
-            shutil.move(os.path.join(source, file), os.path.join(destinations[3], file))
-        elif file.endswith(".txt"):
-            shutil.move(os.path.join(source, file), os.path.join(destinations[4], file))
-        elif file.endswith(".mp4"):
-            shutil.move(os.path.join(source, file), os.path.join(destinations[5], file))
-        elif file.endswith(".png"):
-            shutil.move(os.path.join(source, file), os.path.join(destinations[6], file))
+    try:
+        # Moving files
+        for file in source_files:
+            if file.endswith(".pdf"):
+                shutil.move(os.path.join(source, file), os.path.join(destinations[0], file))
+            elif file.endswith(".exe") or file.endswith(".msi"):
+                shutil.move(os.path.join(source, file), os.path.join(destinations[1], file))
+            elif file.endswith(".mp3"):
+                shutil.move(os.path.join(source, file), os.path.join(destinations[2], file))
+            elif file.endswith(".zip"):
+                shutil.move(os.path.join(source, file), os.path.join(destinations[3], file))
+            elif file.endswith(".txt"):
+                shutil.move(os.path.join(source, file), os.path.join(destinations[4], file))
+            elif file.endswith(".mp4"):
+                shutil.move(os.path.join(source, file), os.path.join(destinations[5], file))
+            elif file.endswith(".png"):
+                shutil.move(os.path.join(source, file), os.path.join(destinations[6], file))
+    except FileNotFoundError as e:
+        '''
+        We break out of this because all the files have been sorted, but because of the progress bar, it repeats
+        the file sorting system again
+        '''
+        print(e)
+        print("Files Sorted!")
+        break
     
 print("Files sorted!")
