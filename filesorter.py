a5794d88b59bc2fda67d7d9022409bf477eec668
YanniTheDev <71956500+YanniTheDev@users.noreply.github.com>
2025-01-07 15:51:09 +1300

    added a progress bar!

--- filesorter.py
+++ filesorter.py
@@ -1,11 +1,13 @@
 import os
 import shutil
 
+from tqdm import tqdm
+
 # Specifying source paths where the unsorted files are
 with open("sourcepath.txt") as source_path:
     source = source_path.readline()
 
-source_destination = os.listdir(source)
+source_files = os.listdir(source)
 
 # Specifying where our sorted files will end up
 with open("destinationpath.txt") as destination_path:
@@ -14,13 +16,18 @@ with open("destinationpath.txt") as destination_path:
 # Separating the destinations into each individual destination
 destinations = destinations.split(",")
 
-# Moving files
-for file in source_destination:
-    if file.endswith(".pdf"):
-        shutil.move(os.path.join(source, file), os.path.join(destinations[0], file))
-    elif file.endswith(".exe") or file.endswith(".msi"):
-        shutil.move(os.path.join(source, file), os.path.join(destinations[1], file))
-    elif file.endswith(".mp3"):
-        shutil.move(os.path.join(source, file), os.path.join(destinations[2], file))
-    elif file.endswith(".zip"):
-        shutil.move(os.path.join(source, file), os.path.join(destinations[3], file))
+# Displaying the progress bar
+for i in tqdm (range(len(source_files)), desc="Sorting..."):
+
+    # Moving files
+    for file in source_files:
+        if file.endswith(".pdf"):
+            shutil.move(os.path.join(source, file), os.path.join(destinations[0], file))
+        elif file.endswith(".exe") or file.endswith(".msi"):
+            shutil.move(os.path.join(source, file), os.path.join(destinations[1], file))
+        elif file.endswith(".mp3"):
+            shutil.move(os.path.join(source, file), os.path.join(destinations[2], file))
+        elif file.endswith(".zip"):
+            shutil.move(os.path.join(source, file), os.path.join(destinations[3], file))
+    
+print("Files sorted!")
